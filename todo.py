9de74c9e509aae8afb7d236b67b1e7c67d5b19b9
AartiM03 <130819690+AartiM03@users.noreply.github.com>
2023-07-15 16:45:47 +0530

    Update todo.py

--- todo.py
+++ todo.py
@@ -71,7 +71,7 @@ while True:
     print("4. Sort tasks")
     print("5. Exit")
 
-    choice = input("Enter your choice (1-4): ")
+    choice = input("Enter your choice (1-5): ")
 
     if choice == "1":
         add_task()
